3264bc363546545d14437ce7924f90c604f705f7
Victor Stepanov <vix522430@bk.ru>
2022-11-18 17:10:03 +0300

    installedroutes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './features/store';
 import App from './components/app/App';
+import { store } from './features/store';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
-
-
